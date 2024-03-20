439ab471086531b19f5f80133b21328dfb6c9eaf
leoncool23 <125378580+leoncool23@users.noreply.github.com>
2024-03-20 18:49:50 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -106,14 +106,8 @@ export const CN_MASKS: BuiltinMask[] = [
         id: "work-0",
         role: "user",
         content:
-          "Help me ensure that the grammar and the spelling is correct. Do not try to polish the text, if no mistake is found, tell me that this paragraph is good. If you find grammar or spelling mistakes, please list mistakes you find in a two-column markdown table, put the original text the first column, put the corrected text in the second column and highlight the key words you fixed.  Finally, please provide the proofreaded text.                        Paragraph: How is you? Do you knows what is it?
-        | Original sentence | Corrected sentence |
-        | :--- | :--- |
-        | How **is** you? | How **are** you? |
-        | Do you **knows** what **is** **it**? | Do you **know** what **it** **is** ? |
-         
-         Below is a paragraph from an academic paper. 
-         You need to report all grammar and spelling mistakes as the example before.",
+          "Act as an English grammar tutor, help me ensure the grammar and the spelling is correct. Do not try to polish the text, if no mistake is found, tell me that this paragraph is good. If you find grammar or spelling mistakes, please list mistakes you find in a two-column markdown table, put the original text the first column, put the corrected text in the second column and highlight the key words you fixed.
+Below is a paragraph from an academic paper. You need to report all grammar and spelling mistakes as the example before.",
         date: "",
       },
     ],
