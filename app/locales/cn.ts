457eae5bbc25b4c9265e330165a7366f527c449a
leoncool23 <125378580+leoncool23@users.noreply.github.com>
2024-03-20 17:55:44 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -360,7 +360,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "我是塞亚AI科研助手，一起开启科研之旅。",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
