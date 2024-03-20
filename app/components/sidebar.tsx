87abca85142c0f10f467d9a9223efa541164e685
leoncool23 <125378580+leoncool23@users.noreply.github.com>
2024-03-20 18:01:42 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          赛亚AI（SciYield AI）
+          赛亚AI科研助手
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          你的AI科研助手
+        SciYield AI，你的AI科研助手
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
